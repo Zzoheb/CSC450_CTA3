9e6f853054407d3bdcedf6fe1d919524e4830fd7
Zoheb Khan <zoheb.khan@csuglobal.edu>
2024-09-17 04:38:29 -0700

    v2 update

--- src/CSC450_CTA3.cpp
+++ src/CSC450_CTA3.cpp
@@ -1,54 +1,76 @@
+/*--------------------------------------------------------
+Program Name: Integer Pointer Program
+Author: Zoheb Khan
+Date: 08/28/24
+----------------------------------------------------------
+Pseudocode:
+//Mutex for synchronizing output
+//Lock the mutex
+//Lock the mutex
+//Ask the user for three integer values
+//Dynamically allocate memory for the three variables
+//Store the values in the dynamically allocated memory
+//Create three threads, passing the pointers to the functions
+//Wait for the threads to finish execution
+//Deallocate the memory
+----------------------------------------------------------
+Program Inputs: Three user input integer values
+Program Outputs: The values of the pointers
+--------------------------------------------------------*/
+#include <windows.h>  // For Windows thread functions
 #include <iostream>
 #include <thread>
 #include <mutex>
 
-std::mutex mtx;  // Mutex for synchronizing output
+std::mutex mtx;  //Mutex for synchronizing output
 
-void thread1(int* ptr1) {
-    std::lock_guard<std::mutex> lock(mtx);  // Lock the mutex
-    std::cout << "Thread 1 pointer value: " << *ptr1 << std::endl;
-}
+// Function to assign the thread to a specific core and print its pointer value
+void threadTask(int* ptr, DWORD_PTR coreMask) {
+    HANDLE current_thread = GetCurrentThread();  //Get the current thread handle
 
-void thread2(int* ptr2) {
-    std::lock_guard<std::mutex> lock(mtx);  // Lock the mutex
-    std::cout << "Thread 2 pointer value: " << *ptr2 << std::endl;
-}
+    //Set the thread's CPU affinity to the specified core using coreMask
+    if (SetThreadAffinityMask(current_thread, coreMask) == 0) {
+        std::cerr << "Error setting thread affinity!" << std::endl;
+    }
 
-void thread3(int* ptr3) {
-    std::lock_guard<std::mutex> lock(mtx);  // Lock the mutex
-    std::cout << "Thread 3 pointer value: " << *ptr3 << std::endl;
+    //Lock the mutex to synchronize output
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << "Thread running on core with mask: " << coreMask << " and pointer value: " << *ptr << std::endl;
+    }
 }
 
 int main() {
-    // Ask the user for three integer values
+    //Ask the user for three integer values
     int val1, val2, val3;
     std::cout << "Enter three integer values: " << std::endl;
     std::cin >> val1 >> val2 >> val3;
 
-    // Dynamically allocate memory for the three variables
+    //Dynamically allocate memory for the three variables
     int* ptr1 = new int;
     int* ptr2 = new int;
     int* ptr3 = new int;
 
-    // Store the values in the dynamically allocated memory
+    //Store the values in the dynamically allocated memory
     *ptr1 = val1;
     *ptr2 = val2;
     *ptr3 = val3;
 
-    // Create three threads, passing the pointers to the functions
-    std::thread t1(thread1, ptr1);
-    std::thread t2(thread2, ptr2);
-    std::thread t3(thread3, ptr3);
+    //Core 0 has a mask of 0x1, Core 1 has a mask of 0x2, Core 2 has a mask of 0x4
+    std::thread t1(threadTask, ptr1, 0x1);  //Assign thread to core 0
+    std::thread t2(threadTask, ptr2, 0x2);  //Assign thread to core 1
+    std::thread t3(threadTask, ptr3, 0x4);  //Assign thread to core 2
 
-    // Wait for the threads to finish execution
+    //Wait for the threads to finish execution
     t1.join();
     t2.join();
     t3.join();
 
-    // Deallocate the memory
+    //Deallocate the memory
     delete ptr1;
     delete ptr2;
     delete ptr3;
 
     return 0;
 }
+
